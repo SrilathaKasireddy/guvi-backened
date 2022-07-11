d7069b54ecc35fe6351799a7e27c26f7d4bd0369
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-07-11 14:20:28 +0530

    connected  to mongo

--- file.js
+++ file.js
@@ -1,47 +1,47 @@
  const fs=require("fs");
-//create
-// fs.writeFile("./awesome.html",quote,(err)=>{
-//  console.log("completed writing")})
+create
+fs.writeFile("./awesome.html",quote,(err)=>{
+ console.log("completed writing")})
 
 
- // for(let i=1;i<=10;i++){
- //  fs.writeFile(`./backup/text-${i}.html`,quote,(err)=>{
- //   console.log(`completed writing text-${i}.html`)})
+ for(let i=1;i<=10;i++){
+  fs.writeFile(`./backup/text-${i}.html`,quote,(err)=>{
+   console.log(`completed writing text-${i}.html`)})
   
- // }
- // const[ , ,noffiles]=process.argv
- // for(let i=1;i<=noffiles;i++){
- //  fs.writeFile(`./backup/text-${i}.html`,quote,(err)=>{
- //   console.log(`completed writing text-${i}.html`)})
+ }
+ const[ , ,noffiles]=process.argv
+ for(let i=1;i<=noffiles;i++){
+  fs.writeFile(`./backup/text-${i}.html`,quote,(err)=>{
+   console.log(`completed writing text-${i}.html`)})
   
- // }
+ }
 //read
- // fs.readFile("./cool.txt","utf-8",(err,data)=>{
- //  if(err){
- //   console.log(err);
+ fs.readFile("./cool.txt","utf-8",(err,data)=>{
+  if(err){
+   console.log(err);
 
- //  }
- //  else{
- //  console.log(data)}
- // })
-
-
+  }
+  else{
+  console.log(data)}
+ })
 
 
- // const quote="now or never"
-//update
- // fs.appendFile("./nice.txt","\n"+quote,(err)=>{
- //  console.log("completed writing")
- // })
+
+
+ const quote="now or never"
+update
+ fs.appendFile("./nice.txt","\n"+quote,(err)=>{
+  console.log("completed writing")
+ })
  
 
-//delete
+delete
 
- // fs.unlink("./delete.css",err=>{
- //  if(err){console.log(err)
- //  }else{
- //  console.log("completed deleting")
- // }})
+ fs.unlink("./delete.css",err=>{
+  if(err){console.log(err)
+  }else{
+  console.log("completed deleting")
+ }})
 
 
 
