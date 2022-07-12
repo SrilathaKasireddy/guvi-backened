408c21986e0dc723c628bd590732196212a7682b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-07-12 20:01:26 +0530

    added _id

--- routes/helper.js
+++ routes/helper.js
@@ -1,16 +1,23 @@
 import { client } from "../index.js";
+import { ObjectId } from "mongodb";
 
 export async function createNewMovies(data) {
-    return await client.db("Movies").collection("Movies").insertMany(data);
+    return await client
+    .db("Movies")
+    .collection("Movies").
+    insertMany(data);
 }
 export async function updateMovieById(id, data) {
-    return await client.db("Movies").collection("Movies").updateOne({ id: id }, { $set: data });
+    return await client
+    .db("Movies")
+    .collection("Movies")
+    .updateOne({_id: ObjectId(id )}, { $set: data });
 }
 export async function deleteMovieById(id) {
     return await client.db("Movies").collection("Movies").deleteOne({ id: id });
 }
 export async function getMovieById(id) {
-    return await client.db("Movies").collection("Movies").findOne({ id: id });
+    return await client.db("Movies").collection("Movies").findOne({ _id: ObjectId(id )});
 }
 export async function getAllMovies(request) {
     return await client.db("Movies").collection("Movies").find(request.query).toArray();
